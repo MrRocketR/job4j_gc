71245f1c533691fb958438cb7cb61825bfff9eb8
MrRocketR <kuptovns@gmail.com>
2022-06-21 22:48:37 +0300

    1. Реализация кеша на SoftReference

---
+++ src/main/java/ru/job4j/gc/cache/DirFileCache.java
@@ -0,0 +1,20 @@
+package ru.job4j.gc.cache;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key) {
+        Path dir = Path.of(key);
+        return null;
+    }
+
+}
---
+++ src/main/java/ru/job4j/gc/cache/Emulator.java
@@ -0,0 +1,17 @@
+package ru.job4j.gc.cache;
+
+public class Emulator {
+    private AbstractCache<String, String> abstractCache;
+    private DirFileCache dirFileCache;
+
+}
+
+
+/*
+- указать кэшируемую директорию
+
+- загрузить содержимое файла в кэш
+
+- получить содержимое файла из кэша
+
+ */
